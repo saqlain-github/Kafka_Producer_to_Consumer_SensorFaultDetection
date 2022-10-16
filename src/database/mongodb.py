917e4711e0511287c5c16f762acbcd70cfcc70e8
saqlain-github <syedsaqlainahmed43@gmail.com>
2022-10-16 23:09:22 +0530

    added working code for Producer and Consumer

--- src/database/mongodb.py
+++ src/database/mongodb.py
@@ -8,8 +8,9 @@ ca = certifi.where()
 class MongodbOperation:
 
     def __init__(self) -> None:
-
-        self.client = pymongo.MongoClient(os.getenv('MONGO_DB_URL'),tlsCAFile=ca)
+        MONGO_DB_URL = "mongodb://localhost:27017/"
+        #tlsCAFile=ca,
+        self.client = pymongo.MongoClient(MONGO_DB_URL)
         self.db_name="ineuron"
 
     def insert_many(self,collection_name,records:list):
--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -1,7 +1,7 @@
 
 import os
 
-
+'''
 SECURITY_PROTOCOL="SASL_SSL"
 SSL_MACHENISM="PLAIN"
 API_KEY = os.getenv('API_KEY',None)
@@ -12,7 +12,20 @@ SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
 SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
 SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
 SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
+'''
 
+SECURITY_PROTOCOL="SASL_SSL" #Simple Auth and Security layer - connect securely
+SSL_MACHENISM="PLAIN"
+
+#API Details
+API_KEY = "COW5763J2HJXHT7E" #os.getenv('API_KEY',None)
+API_SECRET_KEY = "rWe19ZGl0FZZRh4jmk9XLetDeDzDFFpZkBEX+xNHnHLrx3E3wMdZrJgay04K5iyJ" # os.getenv('API_SECRET_KEY',None)
+BOOTSTRAP_SERVER = "pkc-41p56.asia-south1.gcp.confluent.cloud:9092" #os.getenv('BOOTSTRAP_SERVER',None)
+
+# Schema Related Cred
+ENDPOINT_SCHEMA_URL  = "https://psrc-znpo0.ap-southeast-2.aws.confluent.cloud" #os.getenv('ENDPOINT_SCHEMA_URL',None)
+SCHEMA_REGISTRY_API_KEY = "56HGY64GFCDTVH73" #os.getenv('SCHEMA_REGISTRY_API_KEY',None)
+SCHEMA_REGISTRY_API_SECRET = "VTWaZjmR7nn8bD6HvxDBydlP2/N2oq+BVDOlvoM6EFPDx2+CUPgSAI6Pm478iNkI" #os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
 
 def sasl_conf():
 
